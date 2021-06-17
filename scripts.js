562d820efe6c17ce951a9af2616aad6975b4c454
rubensato <rubensato@gmail.com>
2021-06-17 07:00:02 -0300

    movimiento de ficha y cambio de turno

--- scripts.js
+++ scripts.js
@@ -33,7 +33,7 @@ var seleccionCelda = e => {
     
     console.log('posicion anterior: ' + posicionMarcada);
 
-    if (e.target.classList.contains('ficha-blanca')){
+    if (e.target.classList.contains('ficha-blanca')){          //----->>>>> seleccion de ficha blanca
         if (turnoJugador == 1) {
             if(e.target.classList.contains('casilla-seleccionada') !== true){
                 e.target.classList.add("casilla-seleccionada");
@@ -49,7 +49,7 @@ var seleccionCelda = e => {
             return;
         }
 
-    } else if (e.target.classList.contains('ficha-negra')){
+    } else if (e.target.classList.contains('ficha-negra')){     //----->>>>> seleccion de ficha negra
         if (turnoJugador == 2) {
             if(e.target.classList.contains('casilla-seleccionada') !== true){
                 e.target.classList.add("casilla-seleccionada");
@@ -64,7 +64,41 @@ var seleccionCelda = e => {
             window.alert(mensaje);
             return;
         }
+    } else {                                            //----->>>>> seleccion de ficha casilla vacia
+
+        if (posicionMarcada !== "ninguna") {            //----->>>>> verifica si hay una casilla con ficha marcada
+            var casillaDesmarca = document.getElementById(posicionMarcada);
+            
+            //Ubico las fichas segun la posicion de la celda, si esta en alguno de los arreglos de posicion
+            if ( casillaDesmarca.classList.contains('ficha-blanca') && turnoJugador == 1 ) { 
+                //si verifico que hanía una casilla seleccionada con ficha blanca y es turno de las blancas
+                //muevo la ficha a la nueva casilla
+                e.target.classList.add("ficha-blanca");     
+
+                //quito la fichan de la casilla anterior
+                casillaDesmarca.classList.remove("ficha-blanca");  
+                //quito la marca de seleccion de casilla
+                casillaDesmarca.classList.remove("casilla-seleccionada");  
+                
+                //asigno turno al otro jugador
+                turnoJugador = 2;         
+            }
+            else if(casillaDesmarca.classList.contains('ficha-negra') && turnoJugador == 2 ) {
+                //si verifico que hanía una casilla seleccionada con ficha negraa y es turno de las negras
+                //muevo la ficha a la nueva casilla
+                e.target.classList.add("ficha-negra"); 
+
+                //quito la fichan de la casilla anterior
+                casillaDesmarca.classList.remove("ficha-negra");  
+                //quito la marca de seleccion de casilla
+                casillaDesmarca.classList.remove("casilla-seleccionada"); 
+                
+                //asigno turno al otro jugador
+                turnoJugador = 1;
+            }
+        }
     }
+
 
     console.log('posicion nueva: ' + posicionMarcada);
 
