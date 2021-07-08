ec5e027f8e46f907dc6ba906d536d4d819b918c7
rubensato <rubensato@gmail.com>
2021-07-08 08:22:44 -0300

    validacion de datos del formulario de contacto

--- contacto.js
+++ contacto.js
@@ -18,26 +18,51 @@ function enviarDatosContacto() {
     var contactoMail = campoMail.value;
     var contactoConsulta = campoConsulta.value;
 
+    //creo el arreglo de datos a enviar
     var datosEnviar = {
         contactoNombre,
         contactoMail,
         contactoConsulta
     }
+    //nombre del servidor al que le enviare los datos
     var servidor = 'https://jsonplaceholder.typicode.com/posts'
 
+    //VALIDACIONES DE LOS DATOS DE LOS CAMPOS
+    //verifico que el texto de nombre y apellido ingresado tenga al menos 5 caracteres
+    if(contactoNombre.length >= 5){     
+        //verifico que la direccion de mail tenga una estructura valida
+        if(contactoMail.indexOf("@") > 0 && contactoMail.length >= 9 && (contactoMail.lastIndexOf(".") - contactoMail.indexOf("@")) > 3){           
+            //verifico que el campo de la consulta no este vacio
+            if(contactoConsulta.length >= 10){   
+                
+                //ejecuto el envio de datos
+                //script de envio POST tomato del sitio API gratuita https://jsonplaceholder.typicode.com
+                fetch('https://jsonplaceholder.typicode.com/posts', {
+                    method: 'POST',
+                    body: JSON.stringify(datosEnviar)
+                })
+                .then((response) => response.json())
+                .then((json) => console.log('Datos enviados al servidor: Nombre: ' 
+                    + contactoNombre + ' cuyo mail es: '
+                    + contactoMail + ' ,y su consulta es: '
+                    + contactoConsulta))
+                .then((json) => window.alert('Los datos de la consulta fueron enviados'))
+                .catch(err => console.log('Error al enviar al servidor: ' + err))
+                .catch(err => window.alert('Error al enviar al servidor: ' + err));
+                  
+                
+            }
+            else{
+                window.alert('Datos incompletos: Escriba la consulta a realizar');
+            }
+        }
+        else{
+            window.alert('Datos incompletos: La direccion de correo electronico no es correcta');
+        }
+    }
+    else{
+        window.alert('Datos incompletos: Escriba su nombre y apellido, minimo 5 caracteres');
+    }	
 
-    //script de envio POST tomato del sitio API gratuita https://jsonplaceholder.typicode.com
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify(datosEnviar)
-    })
-    .then((response) => response.json())
-    .then((json) => console.log('Datos enviados al servidor: Nombre: ' 
-        + contactoNombre + ' cuyo mail es: '
-        + contactoMail + ' ,y su consulta es: '
-        + contactoConsulta))
-    .then((json) => window.alert('Los datos de la consulta fueron enviados'))
-    .catch(err => console.log('Error al enviar al servidor: ' + err))
-    .catch(err => window.alert('Error al enviar al servidor: ' + err));
 
 }
